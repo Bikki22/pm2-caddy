3afde5a01d94659afde55bb5c2c927fc7b0e31d0
Bikki chaudhary <cbikki35@gmail.com>
2026-03-19 22:52:42 +0545

    simple code added

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 import express from "express";
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from server" });
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running in port ${PORT}`);
+});
